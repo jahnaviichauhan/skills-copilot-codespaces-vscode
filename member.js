d9e990159828098a6d512745679dd5d3d2e05ef3
Jahnavi Singh Chauhan <jahnavi.singhchauhan@studentambassadors.com>
2023-07-25 12:49:41 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember(){
+    return{
+        restrict: 'E',
+        templateURL: 'modules/skills/views/member.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: '='
+        }
+    };
+}
